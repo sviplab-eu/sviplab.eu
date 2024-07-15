017531d643221f1370fbb349289c81c921f05e00
pilyav4ik <ipiliavskyi@gmail.com>
2024-07-15 15:27:02 +0200

    #47 Added images grid to project page

--- src/app/components/project/index.tsx
+++ src/app/components/project/index.tsx
@@ -8,13 +8,13 @@ export default function index({index, title, previewImageURL, services, setModal
         <div className='m-0 md:m-5 hover:m-8 ease-in-out duration-300 cursor-none' onMouseEnter={() => { setModal({ active: true, index }) }} onMouseLeave={() => { setModal({ active: false, index }) }}>
             
             <Image 
-                    src={`/images/${previewImageURL}`}
+                    src={`${previewImageURL}`}
                     width={1000}
                     height={0}
                     alt="image"
                     />
             <div className='flex items-center pt-5'>
-                <h2 className='pr-3'>{title}</h2>
+                <h2 className='pr-3 text-xl font-bold'>{title}</h2>
                 
             </div>
             </div>
